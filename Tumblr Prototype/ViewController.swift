1cbe3955be4e86e2c9af029899fc3f7596865dae
Bjørn Rostad <hi@bjornrostad.no>
2014-10-10 18:09:16 -0700

    Initial Commit

---
+++ Tumblr Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr Prototype
+//
+//  Created by Bjørn Eivind Rostad on 10/10/14.
+//  Copyright (c) 2014 Bjørn Eivind Rostad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
