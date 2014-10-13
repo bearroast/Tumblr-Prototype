a7dc07fe40ea139150bb293d7f09fd95a71af03b
Bjørn Rostad <hi@bjornrostad.no>
2014-10-13 12:46:01 -0700

    hooked up the home view to viewDidLoad

--- Tumblr Prototype/ViewController.swift
+++ Tumblr Prototype/ViewController.swift
@@ -12,9 +12,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var containerView: UIView!
 
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var composeButton: UIButton!
+    @IBOutlet weak var accountButton: UIButton!
+    @IBOutlet weak var trendingButton: UIButton!
+    
+    
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var composeViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController") as UIViewController
+        var searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController") as UIViewController
+        var composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController") as UIViewController
+        var accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController") as UIViewController
+        var trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController") as UIViewController
+        
+        containerView.addSubview(homeViewController.view)
+        
+        homeButton.selected = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
