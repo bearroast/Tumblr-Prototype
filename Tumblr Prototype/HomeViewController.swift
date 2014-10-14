1f7d1de50b4ce6db6891e9e3863bceeca95992b3
Bjørn Rostad <hi@bjornrostad.no>
2014-10-14 01:23:56 -0700

    basics of the login working

--- Tumblr Prototype/HomeViewController.swift
+++ Tumblr Prototype/HomeViewController.swift
@@ -8,14 +8,76 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
 
+    var isPresenting: Bool = true
+    var loginViewController: UIViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        loginViewController = storyboard.instantiateViewControllerWithIdentifier("LoginViewController") as UIViewController
     }
 
+        
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        var destinationVC = segue.destinationViewController as UIViewController
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationVC.transitioningDelegate = self
+        
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+        
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 0.4
+    }
+        
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        if (isPresenting) {
+            
+            let vc = toViewController as LoginViewController
+
+            
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                toViewController.view.alpha = 1
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+            }
+        } else {
+            
+            let vc = fromViewController as LoginViewController
+
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
+    }
+        
+        
+    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Tumblr Prototype/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Tumblr Prototype
+//
+//  Created by Bjørn Eivind Rostad on 10/14/14.
+//  Copyright (c) 2014 Bjørn Eivind Rostad. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emailField.becomeFirstResponder()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCloseButton(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+
+    }
+}
